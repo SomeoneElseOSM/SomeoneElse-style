34b795f52313fc1378df1eec768c0e71564cd4aa
Andy Townsend <ajtown@atownsend.org.uk>
2020-11-29 14:26:04 +0000

    Updated taginfo with more information, and fixed some copy and paste faux pas.  Also updated script with more comments.

--- taginfo_lua_genkeylist.sh
+++ taginfo_lua_genkeylist.sh
@@ -2,6 +2,12 @@
 # This script extracts key names used by SomeoneElse-style/style.lua
 # for use in a taginfo project file.
 #
+# The results are designed to be compared with taginfo_lua_nodesc.txt to see
+# what has changed, and those changes should then be manually changed in
+# taginfo.json.  The change has to be manual as the "description" in the .json
+# can't be generated from anywhere.  Changes to the use of keys in the .lua
+# will also require changes to the json, and this must also be done manually.
+#
 # Example taginfo project file with lots in it:
 # https://raw.githubusercontent.com/mapbox/mapbox-navigation-ios/master/taginfo.json
 # ------------------------------------------------------------------------------
