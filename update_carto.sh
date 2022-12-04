4b5e55c741b72c9f9a77b6f39097218503f7ec05
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2022-12-04 01:26:32 +0000

    Updated update_carto.sh to match update_render.sh

--- update_carto.sh
+++ update_carto.sh
@@ -10,21 +10,41 @@
 # On Debian 11 or above and Ubuntu 21.04 and above,
 # "local_renderd_user" will probably be "_renderd"
 #
-local_filesystem_user=renderaccount
-local_renderd_user=renderaccount
+local_filesystem_user=ajtown
+local_renderd_user=_renderd
 #
-# First things first - is another copy of the script already running?
+# No reference to "local_database=gis3" here, but see
+# "rm -rf /var/lib/mod_tile/ajt3/?" below.
+#
+# First things first - define some shared functions
+#
+final_tidy_up()
+{
+    cd /home/${local_filesystem_user}/data
+    rm update_render.running
+}
+
+#
+# Before anything else, is an update script for the previous database still running?
+#
+if [[ -f /var/cache/renderd/pyosmium.gis/call_pyosmium.running ]]
+then
+    echo "call_pyosmium still running; /var/cache/renderd/pyosmium.gis/call_pyosmium.running exists"
+    exit 1
+fi
+#
+# Next, is another copy of the script already running?
 #
 cd /home/${local_filesystem_user}/data
 if test -e update_render.running
 then
     echo update_render.running exists so exiting
-    exit
+    exit 1
 else
     touch update_render.running
 fi
 #
-# Next get the latest versions of each part of the map style
+# Next get the latest versions of each part of the map style.
 #
 # This is run from sudo without a connection to an authentication agent, 
 # so it makes sense for the git config url to be "https" and the pushurl "git".  
@@ -41,16 +61,29 @@ sudo -u ${local_filesystem_user} git pull
 cd /home/${local_filesystem_user}/src/openstreetmap-carto-AJT
 pwd
 sudo -u ${local_filesystem_user} git pull
+#
+# We create 3 XML files:
+#
+# The normal mapnik.xml, which uses database gis.
+# An identical mapnik3.xml, but which uses database gis3.
+#
+# Also, a "novispaths" mapnik.xml, based on the very simple layers at
+# https://github.com/SomeoneElseOSM/openstreetmap-carto-AJT/tree/master/novispaths
+# This is a separate tile layer so that it can be used as an overlay.
+#
 carto project.mml > mapnik.xml
+sed "s/\[gis\]/[gis3]/" mapnik.xml > mapnik3.xml
+cd novispaths
+carto project.mml > mapnik.xml
+cd ..
 #
 /etc/init.d/renderd restart
 /etc/init.d/apache2 restart
 #
-rm -rf /var/lib/mod_tile/ajt/?
-rm -rf /var/lib/mod_tile/ajt/??
+rm -rf /var/lib/mod_tile/ajt3/?
+rm -rf /var/lib/mod_tile/ajt3/??
 #
 # And final tidying up
 #
-cd /home/${local_filesystem_user}/data
-rm update_render.running
+final_tidy_up
 #
