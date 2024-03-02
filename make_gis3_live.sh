d110e817b4a86eda1f90ad6c942ad989572a8931
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2024-03-02 20:18:30 +0000

    Updated copyright from 2023 to 2024

--- make_gis3_live.sh
+++ make_gis3_live.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 # make_gis3_live.sh
 #
-# Copyright (C) 2018-2023  Andy Townsend
+# Copyright (C) 2018-2024  Andy Townsend
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- transform_cy.lua
+++ transform_cy.lua
@@ -1,7 +1,7 @@
 -- ----------------------------------------------------------------------------
 -- transform_cy.lua
 --
--- Copyright (C) 2018-2023  Andy Townsend
+-- Copyright (C) 2018-2024  Andy Townsend
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
